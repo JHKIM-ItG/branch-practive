b8a80d513f5ca9dcb583bd8d883bd4bed3db8459
JHKIM-ItG <past7080@gmail.com>
2025-08-22 12:24:58 +0900

    feat: update print fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,9 @@
 for i in range(1,17+1):
-	if i%3 == 0:
-		print(f'{i}:fizz')
+	if i%15 ==0:
+		print(f"{i}:fizzbuzz")
+	elif i%3 == 0:
+		print(f"{i}:fizz")
 	elif i%5 ==0:
-		print(f'{i}:buzz')
+		print(f"{i}:buzz")
 	else:
 		print(i)
